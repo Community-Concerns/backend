a7f50065fdc9fb1236de67840240d07e7ab9ae4c
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-12-21 17:18:42 -0800

    wrote migrations

--- database/migrations/20201221152656_create-tables.js
+++ database/migrations/20201221152656_create-tables.js
@@ -1,10 +1,66 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable("users", tbl => {
+    tbl.increments("id"); 
+    tbl.string("email", 255).notNullable().unique(); 
+    tbl.string("password", 255).notNullable(); 
+  })
+  .createTable("tickets", tbl => {
+    tbl.increments("id"); 
+    tbl.integer("user_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("users"); 
+    tbl.string("title", 255).notNullable(); 
+    tbl.string("description", 255).notNullable(); 
 
+  })
+  .createTable("ticket_upvotes", tbl => {
+    tbl.increments("id"); 
+    tbl.integer("user_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("users");
+    tbl.integer("ticket_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("tickets");
+    
+  })
+  .createTable("comments", tbl => {
+    tbl.increments("id"); 
+    tbl.string("comment", 255).notNullable(); 
+    tbl.integer("ticket_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("tickets");
+    tbl.integer("user_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("users");
+
+  })
+  .createTable("ticket_images", tbl => {
+    tbl.increments("id"); 
+    tbl.integer("ticket_id")
+    .unsigned()
+    .notNullable()
+    .references("id")
+    .inTable("tickets");
+    tbl.string("image_url", 255).notNullable(); 
+    tbl.string("public_id", 255).notNullable(); 
   })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("users")
+  return knex.schema.dropTableIfExists("ticket_images")
+  .dropTableIfExists("comments")
+  .dropTableIfExists("ticket_upvotes")
+  .dropTableIfExists("tickets")
+  .dropTableIfExists("users")
 };
