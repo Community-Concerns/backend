a90d6827cba260136fa9a06a67544c6792e904c1
James <info@thecodediver.com>
2021-01-02 14:51:33 -0500

    display error

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -18,12 +18,12 @@ router.post("/register", validRegisterRequest, async (req, res) => {
   const rounds = 10
   const hash = bcryptjs.hashSync(credentials.password, rounds)
   credentials.password = hash
+  console.log("Addess", process.env.GMAIL_ADDRESS)
+  console.log("Password", process.env.GMAIL_PASSWORD)
+  console.log("Something", transporter)
   try {
     const newUser = await Auth.addUser(credentials)
     if (newUser) {
-      console.log("Addess", process.env.GMAIL_ADDRESS)
-      console.log("Password", process.env.GMAIL_PASSWORD)
-      console.log("Something", transporter)
       const mailOptions = {
         from: 'communityconcernsapp@gmail.com',
         to: credentials.email,
