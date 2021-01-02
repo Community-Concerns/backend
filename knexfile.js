30f3fe26d00c53aa7d9230c68254b9711c07671e
James <info@thecodediver.com>
2021-01-02 12:04:31 -0500

    updated database url

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-const pgConnection = process.env.DATABASE_URL
+const pgConnection = process.env.DATABASE_URL || "postgres://ynjyltyccovkyi:3785652e74fe7d63922e0a6145582f6f5727bffc509cb02057e8627988562807@ec2-3-232-240-231.compute-1.amazonaws.com:5432/ddh3n7doh09lgv"
 
 module.exports = {
 
@@ -24,13 +24,13 @@ module.exports = {
     connection: pgConnection,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: "./database/migrations"
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: "./database/seeds"
+      directory: "./database/seeds",
     },
-  }
-};
+  },
+}
